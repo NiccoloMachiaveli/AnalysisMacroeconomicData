bdb87de9865c4c3f1496f62c31f2956fe69c3479
NiccoloMachiaveli <Koroliskander@yandex.ru>
2026-02-10 20:29:59 +0400

    Поменял чтение файлов
    Добавил чтение для нескольких файлов потому что до этого они сохранялись в разных массивах и попытался реализовать avg пока не получилось

--- main.py
+++ main.py
@@ -4,15 +4,42 @@ from tabulate import tabulate
 import pytest
 
 
-def read_economic_files(paths):
-    data = []
-    for path in paths:
+def read_economic_single_file(path):
+    try:
         with open(path, 'r', encoding='utf-8') as file:
-            data.append([row for row in csv.reader(file)])
-    return data
+            reader = csv.reader(file)
+            #headers = next(data)
+            #print(headers)
+            data = list(reader)
+            return data
+    except FileNotFoundError:
+        return []
 
 
-def calculate_avg():
+def read_economic_multiple_files(paths):
+    all_data = []
+
+    for path in paths:
+        data = read_economic_single_file(path)
+
+        if data:
+            all_data.extend(data[1:])
+    return all_data
+
+
+def calculate_avg(data, column_index):
+    total = 0
+    amount = 0
+
+    for i in range(len(data[0])):
+        for row in data[0][i]:
+            try:
+                value = float(row[column_index])
+                total += value
+                amount += 1
+            except (ValueError, IndexError):
+                continue
+    return round(total / amount, 2) if amount > 0 else 0
 
 
 def setup_parser():
@@ -25,8 +52,12 @@ def setup_parser():
     parser.add_argument('--report', type=str, choices=['average-gdp'], required=True, help='Тип создаваемого отчета')
 
     args = parser.parse_args()
-    print(args.files)
-    print(read_economic_files(args.files))
+    data = read_economic_multiple_files(args.files)
+
+    if args.report == 'average-gdp':
+        result = calculate_avg(data, 4)
+
+        print(result)
 
 
 setup_parser()
